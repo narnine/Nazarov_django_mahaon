afc66e40aedc1d453defe7cdcad79cf694c4c66b
narnine <nariman_naazarov@mail.ru>
2018-02-14 15:06:12 +0600

    practice 4

--- news_blog/urls.py
+++ news_blog/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:article_id>/', views.get_article, name='article')
+    path('<int:article_id>/', views.get_article, name='article'),
+    path('new/', views.new_article, name='new_article'),
+    path('<int:article_id>/edit', views.edit_article, name='edit'),
+
 ]
--- news_blog/views.py
+++ news_blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Article
+from .models import Article, Author
 from django.http import HttpResponseNotFound
+from .forms import ArticleForm
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.shortcuts import render, redirect, get_object_or_404
+
+
 
 # Create your views here.
 def index(request):
@@ -31,3 +37,28 @@ articles = [
        'title': 'Second news',
        'text': 'This is the amazing second article'
    }]
+def new_article(request):
+   if request.method == "POST":
+       form = ArticleForm(request.POST)
+       if form.is_valid():
+           article = form.save(commit=False)
+           article.published_date = timezone.now()
+           article.save()
+           return redirect('article', article_id=article.pk)
+   else:
+       form = ArticleForm()
+   return render(request, 'news_blog/edit_article.html', {'form': form})
+
+def edit_article(request, article_id):
+   article = get_object_or_404(Article, pk=article_id)
+   if request.method == "POST":
+       form = ArticleForm(request.POST, instance=article)
+       if form.is_valid():
+           article = form.save(commit=False)
+           article.published_date = timezone.now()
+           article.save()
+           return redirect('article', article_id=article.pk)
+   else:
+       form = ArticleForm(instance=article)
+   return render(request, 'news_blog/edit_article.html', {'form': form})
+
